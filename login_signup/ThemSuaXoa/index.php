4dd833e5cd56e3fe543ef5af0bcb7f8856316415
lethuyhang-humg <2221050137@student.humg.edu.vn>
2024-11-29 00:53:51 +0700

    reload

--- login_signup/ThemSuaXoa/index.php
+++ login_signup/ThemSuaXoa/index.php
@@ -7,9 +7,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Yêu Thích</title>
 </head>
 <body>
+    
     <?php
         if(isset($_GET['page_layout'])){
             switch($_GET['page_layout']){
